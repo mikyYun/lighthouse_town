0bb227b1714ccb9407a7a98a957a86202727a93b
mikyYun <mkyun2714@gmail.com>
2022-06-11 03:25:01 -0700

    orbitcontrol to register animation

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
-// npm i express nodemon socket.io
-
+///////////////////////////////////////
+// Combine cookie / session ????????
+///////////////////////////////////////
 // server running with nodemon : npm run dev
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 // express, morgan, nodemo
 const express = require('express');
+const session = require('express-session')
 const app = express();
 // socket.io
 // step 1. declare socket.id -> run server to check
@@ -15,12 +17,103 @@ const { createServer } = require("http");
 // listen from http, not app
 const httpServer = createServer(app);
 
-const io = Server(httpServer); // optional second params callback
+// console.log("SESSION", session({}))
+// const middleWareSession = session({
+//   secret: "secret_session_key",
+//   resave: false,
+//   saveUninitialized: false,
+// })
+// SESSION [Function: session] {
+//   Store: [Function: Store],
+//   Cookie: [Function: Cookie],
+//   Session: [Function: Session],
+//   MemoryStore: [Function: MemoryStore]
+// }
+const io = Server(httpServer, {
+  // 처음 가짜 세션 생성
 
+  // allowRequest: (req, callback) => {
+  //   // with HTTP long-polling, we have access to the HTTP response here, but this is not
+  //   // the case with WebSocket, so we provide a dummy response object
+  //   const fakeRes = {
+  //     getHeader() {
+  //       return [];
+  //     },
+  //     setHeader(key, values) {
+  //       req.cookieHolder = values[0];
+  //     },
+  //     writeHead() {},
+  //   };
+  //   sessionMiddleware(req, fakeRes, () => {
+  //     if (req.session) {
+  //       // trigger the setHeader() above
+  //       fakeRes.writeHead();
+  //       // manually save the session (normally triggered by res.end())
+  //       req.session.save();
+  //     }
+  //     callback(null, true);
+  //   });
+  // },
+}); // optional second params callback
 
+const sessionMiddleware = session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }});
+app.use(sessionMiddleware)
+io.use((socket, next) => {
+  sessionMiddleware(socket.request, {}, next);
+  // sessionMiddleware(socket.request, socket.request.res, next); will not work with websocket-only
+  // connections, as 'socket.request.res' will be undefined in that case
+});
+// const sessionMiddleware = session({
+//   secret: "changeit",
+//   resave: false,
+//   saveUninitialized: false
+// });
 // someone connedted
 // socket is json format
 io.on("connection", (socket) => {
+  const session = socket.request.session;
+  session.connections++;
+  session.save();
+  // session.foo = "bar";
+  // session.save();
+  // io.on("connection", (socket) => {
+  // 처음 세션값
+  // set session
+  // if (socket.cookieHolder) {
+    //   headers["set-cookie"] = req.cookieHolder;
+    //   delete req.cookieHolder;
+    // }
+    const req = socket.request
+    socket.on("login", (loginInfo) => {
+    socket.request.session.reload((err) => {
+      if (err) {
+        return console.log("ERROR")
+      }
+      console.log("clicked",socket.request.session)
+    })
+    // 로그인 페이지에서 들어오는 정보
+    console.log("LoginInformation", loginInfo)
+    // req.session.reload((err) => {
+    //   if (err) {
+    //     return socket.disconnect();
+    //   }
+      req.session.count++;
+      req.session.save();
+    //   console.log('newSession', req.session)
+    // })
+    // console.log(socket.request.session)
+    // console.log("Login Information",loginInfo)
+    // session.save((err) => {
+    //   if (err) {
+    //     return console.log("SESSION ERR", err)
+    //   }
+    //   req.session.count++;
+    //   req.session.save()
+    // })
+  })
+
+
+
   const users = {}; // only validated user data 
   // console.log('socket is ', socket)
   console.log("Someone has been connedted!");
@@ -43,6 +136,10 @@ io.on("connection", (socket) => {
 
   socket.on("disconnect", (data) => {
     // console.log(data)
+    req.session.count--;
+    req.session.save();
+
+
     console.log(`${socket.name} has disconnected!!`);
     // users = users.filter(name => name !== socket.name);
     // socket.broadcast.emit("DISCONNECT", socket.name); // 전체
