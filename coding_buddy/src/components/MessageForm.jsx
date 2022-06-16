de7bc9e9b1b4fa2bf10ff62656562fa8a277ccb3
Moon Choi <choimy1991@gmail.com>
2022-06-16 13:43:28 -0700

    With pascal - Fix online users in dropdown in chat

--- coding_buddy/src/components/MessageForm.jsx
+++ coding_buddy/src/components/MessageForm.jsx
@@ -1,6 +1,6 @@
 import { useState, useCallback, useContext } from "react";
-
-import { SocketContext, SOCKET_EVENT } from "./service/socket.js";
+import { SOCKET_EVENT } from "./service/socket.js";
+import { SocketContext } from '../App.js'
 
 //이 컴포넌트는 메시지 입력창에 입력하고 있는 텍스트를 state로 관리합니다.그리고 전송 버튼을 누르면 handleSendMessage함수가 실행되어 SEND_MESSAGE 이벤트를 nickname과 입력한 텍스트 데이터와 함께 소켓 서버로 emit합니다.ChatRoom에서 이 컴포넌트를 import 해줍니다.
 
--- coding_buddy/src/components/Recipient.jsx
+++ coding_buddy/src/components/Recipient.jsx
@@ -1,17 +1,27 @@
-import { useState } from "react";
+import { useState, useContext, useEffect } from "react";
 import Select from 'react-select';
-const options = [
-  { value: 'all', label: 'all' },
-  { value: 'moon', label: 'moon' },
-  { value: 'mike', label: 'mike' },
-  { value: 'heesoo', label: 'heesoo' },
-];
+import { SocketContext } from '../App.js'
 
 function Recipient() {
   const [recipient, setRecipient] = useState(null);
+  const { online } = useContext(SocketContext)
+
+  console.log("ONLINE - RECIPIENT.JSX", online)
+  // const [options, setOptions] = useState([{ value: 'all', label: 'all' }])
+
+  // useEffect(() => {
+  //   console.log("SOCKET is recipient triggered?", socket)//
+
+  //   socket.on("all user names", (obj) => {
+  //     console.log('obj', obj) // only contains online users
+  //     //obj.users = [name1, name2, name3]
+
+  //     // const usersList = []
+  //   })
+  // }, [socket])
 
   return (
-    <div className="card d-flex flex-row align-items-center">
+    <div className="card d-flex flex-row align-items-center" >
       <label htmlFor="user-name-input" style={{ width: 290 }}>
         Recipient
       </label>
@@ -22,10 +32,11 @@ function Recipient() {
         value={recipient}
         defaultValue={recipient}
         onChange={setRecipient}
-        options={options}
+        options={online}
+
       />
     </div>
   );
-}
+};
 
 export default Recipient;
