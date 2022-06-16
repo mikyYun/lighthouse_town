4eda6658dd8f5949634491be870dc58154de2a0d
Moon Choi <choimy1991@gmail.com>
2022-06-15 20:40:05 -0700

    clean up Game.jsx

--- coding_buddy/src/components/Game.jsx
+++ coding_buddy/src/components/Game.jsx
@@ -1,29 +1,23 @@
 import React from "react";
 import Canvas from "./Canvas";
 import "./Game.scss";
-// import Navbar from "./Navbar";
-// import Layout from "./Layout";
+
 import Chat from "./Chat";
 import { useLocation } from "react-router-dom";
 import { useState, useCallback, useRef } from "react";
 
-
 export default function Game(props) {
-  // console.log("game loading")
-  // pass the mapimg as props
-  // console.log("GAME PROPS", props.sendPrivateMessage)
-  // pass the mapimg as props
+
   const sendMessage = props.sendMessage
   const sendPrivateMessage = props.sendPrivateMessage
   const location = useLocation();
+
   // console.log('inside game',location.state)  //username
   // console.log('inside game',location.state.userName)  //username
-
   console.log("LOCATION", location)
   console.log("LOCATION.STATE", location.state)
 
-
-  const prevNickname = useRef(null); // prevNickname 변경은 컴포넌트를 리렌더링 하지않습니다.
+  const prevNickname = useRef(null); 
   const [nickname, setNickname] = useState(props.username);
   const handleSubmitNickname = useCallback(newNickname => {
     prevNickname.current = nickname;
