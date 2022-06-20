8dfe21cfc8d9ee04b88394005284df5eeb7d41c9
mikyYun <mkyun2714@gmail.com>
2022-06-19 18:11:28 -0700

    avatar fix

--- app/src/components/ChatRoom.jsx
+++ app/src/components/ChatRoom.jsx
@@ -1,15 +1,19 @@
 import { useState, useCallback, useEffect, useContext, useRef } from "react";
 import MessageForm from "./MessageForm";
-import Avatar from "./Avatar.jsx"
+import Avatar from "./Avatar.jsx";
 import "./ChatRoom.scss";
-import { SOCKET_EVENT, makePublicMessage, makePrivateMessage } from "./service/socket";
+import {
+  SOCKET_EVENT,
+  makePublicMessage,
+  makePrivateMessage,
+} from "./service/socket";
 import { SocketContext } from "../App.js";
-import { UserListContext } from '../App.js'
+import { UserListContext } from "../App.js";
 
 function ChatRoom(props) {
-  const { socket } = useContext(SocketContext)
+  const { socket } = useContext(SocketContext);
   const { recipient, user } = useContext(UserListContext);
-  const { username } = props
+  const { username } = props;
   const [messages, setMessages] = useState([]);
   const chatWindow = useRef(null);
   const moveScrollToReceiveMessage = useCallback(() => {
@@ -20,12 +24,13 @@ function ChatRoom(props) {
       });
     }
   }, []);
-
+  // console.log("USER!!",user)
   // RECEIVE_MESSAGE 이벤트 콜백: messages state에 데이터를 추가.
   // MessageForm & socket > index.js
   // socketIo.to(roomName).emit("RECEIVE_MESSAGE", responseData);
   const handleReceivePublicMessage = useCallback(
     (pongData) => {
+      // console.log("PONG", pongData)
       const newPublicMessage = makePublicMessage(pongData);
       setMessages((messages) => [...messages, newPublicMessage]);
       moveScrollToReceiveMessage();
@@ -47,7 +52,7 @@ function ChatRoom(props) {
     socket.on("PRIVATE", handleReceivePrivateMessage); // 이벤트 리스너 - 프라이빗 메세지
 
     return () => {
-      socket.disconnect()
+      socket.disconnect();
       // socket.off(SOCKET_EVENT.RECEIVE_MESSAGE, handleReceiveMessage); // 이벤트 리스너 해제
       //@@이거 왜 off 안하고 disconnect로 함?
     };
@@ -61,22 +66,26 @@ function ChatRoom(props) {
       </div>
       <div className="chat-window card" ref={chatWindow}>
         {messages.map((message, index) => {
-          const { username, content, time, user } = message;
-          let recipient = ''
-          message.recipient ? recipient = message.recipient : recipient = 'all'
-
+          const { nickname, content, time, user } = message;
+          let recipient = "";
+          message.recipient
+            ? (recipient = message.recipient)
+            : (recipient = "all");
           return (
             <div key={index} className="d-flex flex-row">
-              {username && <div className="message-nickname">
-                {/* <Avatar url={user.avatar} /> */}
-                {username} to {recipient}: </div>}
-              <div>{content}</div>
-              <div className="time">{time}</div>
+              {nickname && (
+                <div className="message-nickname">
+                  <Avatar url={message.user?.avatar} />
+                  {"  "}
+                  {nickname} to {recipient}: {content}
+                </div>
+              )}
+            <div className="time">{time}</div>
             </div>
           );
         })}
       </div>
-      <MessageForm nickname={username} recipient={recipient} />
+      <MessageForm nickname={username} recipient={recipient} user={user} />
     </div>
   );
 }
