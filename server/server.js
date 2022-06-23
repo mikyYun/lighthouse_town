d0d6209b5d3edcb65bc8ef6e45ca424f24665a91
mikyYun <mkyun2714@gmail.com>
2022-06-23 12:45:37 -0700

    proxy err

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ const sessionMiddleware = session({
   secret: "coding_buddy",
   cookie: { maxAge: 60000 },
 });
-const { getOneUserLanguages } = require("./coding_buddy_db");
+// const { getOneUserLanguages } = require("./coding_buddy_db");
 const { Pool } = require("pg");
 const pool = new Pool({
   user: process.env.PGUSER,
