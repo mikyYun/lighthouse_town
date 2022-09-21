8e369340e141d149b92dd69c5e4b02497d78a334
mikyYun <mkyun2714@gmail.com>
2022-09-21 12:06:24 -0700

    split messages

--- app/src/components/helper/Characters.js
+++ app/src/components/helper/Characters.js
@@ -5,7 +5,7 @@ const facing = {
   down: 0,
   left: 1,
   right: 2
-}
+};
 // facing direction
 const cycleLoop = [0, 1, 2, 3];
 class Characters {
@@ -18,10 +18,10 @@ class Characters {
       currentDirection: config.currentDirection,
       frameCount: config.frameCount,
       avatar: config.avatar
-    }
+    };
     this.img = new Image();
     this.img.src = selectAvatar(this.state.avatar);
-    this.img.style.scale=2;
+    this.img.style.scale = 2;
     this.movement_speed = 10;
     this.width = 63.5;
     this.height = 63.5;
@@ -36,13 +36,13 @@ class Characters {
     if (this.state.frameCount >= this.frameLimit) {
       this.state.frameCount = 0;
     }
-  }
+  };
 
   reset = () => {
     this.state.x = 200;
     this.state.y = 420;
     this.frameCount = 0;
-    this.currentDirection = 0
+    this.currentDirection = 0;
     // this.state = {
     //   username: this.username,
     //   x: 200,
@@ -51,7 +51,7 @@ class Characters {
     //   frameCount: 0,
     //   avatar: this.avatar
     // }
-  }
+  };
 
   // add websocket
   move = (keyCode) => {
@@ -59,34 +59,34 @@ class Characters {
       if (keyCode == 37 && this.state.x > 10) {
         this.state.x -= this.movement_speed;
         this.state.currentDirection = facing.left;
-        this.frameDirection()
+        this.frameDirection();
       }
       if (keyCode == 39 && this.state.x < 1060) {
         this.state.x += this.movement_speed;
         this.state.currentDirection = facing.right;
-        this.frameDirection()
+        this.frameDirection();
       }
     }
     if (this.state.y >= 0 && this.state.y <= 570) {
       if (keyCode == 38 && this.state.y > 10) {
         this.state.y -= this.movement_speed;
         this.state.currentDirection = facing.up;
-        this.frameDirection()
+        this.frameDirection();
       }
       if (keyCode == 40 && this.state.y < 560) {
         this.state.y += this.movement_speed;
         this.state.currentDirection = facing.down;
-        this.frameDirection()
+        this.frameDirection();
       }
     }
-  }
-  
+  };
+
   stop = (keyCode) => {
-    console.log(this.state.x, this.state.y)
+    console.log(this.state.x, this.state.y);
     if (keyCode == 37 || keyCode == 38 || keyCode == 39 || keyCode == 40) {
       this.state.frameCount = 0;
     }
-  }
+  };
 
   drawFrame = (ctx) => {
     const frameX = cycleLoop[this.state.frameCount];
@@ -95,9 +95,9 @@ class Characters {
       this.width, this.height,
       this.state.x, this.state.y,
       this.width, this.height
-    )
+    );
 
-  }
+  };
 
   showName = (ctx) => {
     // name over head
@@ -107,13 +107,13 @@ class Characters {
       this.state - 10,
       80,
       20
-    )
+    );
     ctx.fillStyle = "black";
     ctx.fillText(
       this.state.username,
       this.state.x + 10,
       this.state.y + 5
-    )
+    );
   };
 
 
@@ -123,25 +123,45 @@ class Characters {
       "",
       this.state.x - 5,
       this.state.y - 13
-    )
-  }
+    );
+  };
 
   showChat = (ctx, msg) => {
-    ctx.font = 'bold 30px monospace'
-    ctx.fillStyle = "blue"
-    this.message = msg
+    ctx.font = 'bold 20px monospace';
+    ctx.fillStyle = "blue";
+    // this.message = ""
     // const lineHeight = 30
-    // const lines = msg.split(" ");
-    // for (let i = 0; i < lines.length; i + 2) {
-    //   ctx.fillText(`${lines[i]} ${lines[i + 1]}`, this.state.x, this.state.y - (i * 2 * lineHeight))
-    // }
-    // console.log("CTX", ctx)
-    ctx.fillText(
-      msg,
-      this.state.x - 5,
-      this.state.y - 13, 
+    const lines = msg.split(" ").filter(word => word != "");
+    let messageYPosition = lines.length % 2 === 0 ? (
+      ((lines.length / 2) * 20 - 20)
+      ) : (
+      ((lines.length / 2 - lines.length % (2 / 2)) * 20 - 20)
+      )
+    console.log(messageYPosition)
+    for (let i = 0; i < lines.length; i += 2) {
+      let message;
+      if (lines[i + 1]) {
+        message = `${lines[i]} ${lines[i + 1]}`;
+      } else {
+        message = lines[i];
+      }
+      if (i > 1) {
+        messageYPosition -= 20;
+      }
+      // this.message += lines[i]
+      ctx.fillText(
+        message,
+        this.state.x - 5,
+        this.state.y - messageYPosition - 20,
       );
     }
+  };
+  // ctx.fillText(
+  //   this.message,
+  //   this.state.x - 5,
+  //   this.state.y - messagePosition, 
+  //   );
+  // }
 };
 
 export default Characters;
