c2fb3240367a7d595084448edf020cec1bdd1b19
mikyYun <mkyun2714@gmail.com>
2022-09-22 19:45:19 -0700

    connectionstring

--- server/coding_buddy_db.js
+++ server/coding_buddy_db.js
@@ -3,12 +3,12 @@ require("dotenv").config({silent: true});
 /** USE THIS DB */
 console.log("process.env", process.env)
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
-  // connectionString: process.env.DATABASE_URL,
+  // user: process.env.PGUSER,
+  // host: process.env.PGHOST,
+  // database: process.env.PGDATABASE,
+  // password: process.env.PGPASSWORD,
+  // port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL,
   sslmode: require
 });
 
