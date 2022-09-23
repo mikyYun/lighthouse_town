9146827c878cadd6aed8c8281163a67cd1bb7dac
mikyYun <mkyun2714@gmail.com>
2022-09-22 23:36:12 -0700

    test

--- app/src/components/Online.jsx
+++ app/src/components/Online.jsx
@@ -30,7 +30,7 @@ export default function Online({ changeRecipient }) {
     );
     setOnlineUserNames(filterOnlineList);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [onlineList]);
+  }, [onlineList, onlineUserNames]);
 
   // useEffect(() => {
     // if (!loadLists && onlineUserNames.length > 0) setLoadLists(true);
